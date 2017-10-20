b9192636063ea667c10b6c72f219e320c48299c7
Federico Calore <fedecalo@gmail.com>
2017-10-21 00:15:38 +0200

    implemented command line arguments for group size and removed members

--- gruppi_comunita.py
+++ gruppi_comunita.py
@@ -3,67 +3,87 @@
 import argparse
 import random
 
-membri = (('Ana',),
-          ('Enrique', 'Concha'),
-          ('Ester',),
-          ('Federico', 'Teresa'),
-          ('Giancarlo',),
-          ('Giuseppe', 'Noemi'),
-          ('Isabel',),
-          ('Juanita',),
-          ('Marcelo', 'Chantal'),
-          ('Maria',),
-          ('Marta',),
-          ('Michele', 'Federica'),
-          ('Philippe',),
-          ('Tommaso', 'Laura')
-          )
+all_members = (('Ana',),
+               ('Enrique', 'Concha'),
+               ('Ester',),
+               ('Federico', 'Teresa'),
+               ('Giancarlo',),
+               ('Giuseppe', 'Noemi'),
+               ('Isabel',),
+               ('Juanita',),
+               ('Marcelo', 'Chantal'),
+               ('Maria',),
+               ('Marta',),
+               ('Michele', 'Federica'),
+               ('Philippe',),
+               ('Tommaso', 'Laura')
+               )
 
-#TODO parse arguments to remove names
-da_rimuovere = (('Ana',),)
 
-#TODO parse arguments define group size
-persone_per_gruppo = 5
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--number', type=int, help='Specify how many members per group')
+    parser.add_argument('-r', '--remove', nargs='*', help='Remove members from the list')
+    args = parser.parse_args()
+    return vars(args)
 
-def copia_lista(membri):
-    membri_l = [(x, len(x)) for x in membri]
-    return(membri_l)
 
-def rimuovi_membri(membri_l, da_rimuovere):
-    for nome in da_rimuovere:
-        membri_l.remove((nome, len(nome)))
-    return membri_l
+def copy_list(members):
+    members_list = [(x, len(x)) for x in members]
+    return members_list
 
-def genera_gruppi(membri, persone_per_gruppo, da_rimuovere = None):
-    membri = copia_lista(membri)
-    membri = rimuovi_membri(membri, da_rimuovere)
-    gruppi = []
-    while len(membri) > 0:
-        gruppo, persone = [], 0
-        while persone < persone_per_gruppo and len(membri) > 0:
-            membro = membri.pop(random.randrange(len(membri)))
-            persone += membro[1]
-            gruppo.append(membro[0])
-        gruppi.append(gruppo)
-    return gruppi
 
-def formatta_gruppi(membri, persone_per_gruppo, da_rimuovere):
-    gruppi = genera_gruppi(membri, persone_per_gruppo, da_rimuovere)
-    for i, gruppo in enumerate(gruppi, start=1):
-        print('Gruppo', i, end=': ')
-        componenti = len(gruppo)
-        for i, membro in enumerate(gruppo, start=1):
-            num_pers = len(membro)
+def remove_members(members_list, to_remove):
+    for nome in to_remove:
+        members_list.remove((nome, len(nome)))
+    return members_list
+
+
+def generate_groups(members, members_per_group, to_remove=None):
+    members = copy_list(members)
+    members = remove_members(members, to_remove)
+    groups = []
+    while len(members) > 0:
+        group, people = [], 0
+        while people < members_per_group and len(members) > 0:
+            member = members.pop(random.randrange(len(members)))
+            people += member[1]
+            group.append(member[0])
+        groups.append(group)
+    return groups
+
+
+def format_groups(members, members_per_group, to_remove):
+    group = generate_groups(members, members_per_group, to_remove)
+    for g, group in enumerate(group, start=1):
+        print('Gruppo', g, end=': ')
+        components = len(group)
+        for m, member in enumerate(group, start=1):
+            num_pers = len(member)
             if num_pers == 1:
-                print(membro[0], end='')
+                print(member[0], end='')
             elif num_pers == 2:
-                print(membro[0], '&', membro[1], end='')
+                print(member[0], '&', member[1], end='')
             else:
-                raise ValueError('Sono supportati solo singoli membri o coppie.')
-            if i < componenti: print(', ', end='')
+                raise ValueError('Only single members or couples are supported.')
+            if m < components:
+                print(', ', end='')
         print('')
     return None
 
-def main(): formatta_gruppi(membri, persone_per_gruppo, da_rimuovere)
 
-if __name__ == '__main__': main()
+def main():
+    args = get_arguments()
+    to_remove = []
+    for name in args.get('remove'):
+        name = tuple(name.split(','))
+        to_remove.append(name)
+    to_remove = tuple(to_remove)
+    print('Escludo i seguenti membri dai gruppi: {r}'.format(r=to_remove))
+    members_per_group = args.get('number')
+    print('Genero gruppi di {n} persone'.format(n=members_per_group))
+    format_groups(all_members, members_per_group, to_remove)
+
+
+if __name__ == '__main__':
+    main()
