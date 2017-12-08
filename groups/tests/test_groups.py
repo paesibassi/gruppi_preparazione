174002723db31bbba4c90cbcac77d0a72141577c
Federico Calore <Federico.Calore@ing.nl>
2017-12-09 00:30:03 +0100

    Reorganised files into a package folder structure

--- groups/tests/test_groups.py
+++ groups/tests/test_groups.py
@@ -1,3 +1,5 @@
+"""Unit tests for the groups module, using pytest)"""
+
 import random
 import datetime
 import collections
@@ -5,32 +7,24 @@ import sys
 import argparse
 import pytest
 sys.path.append('.')
-import groups as gr
+import groups.groups as gr
 
-
-def test_get_arguments():
-    # TODO unit test argument capturing
-    pass
-    # args: -f persone.json -n 3  -w saturday
-    # vars = gr.get_arguments()
-    # assert vars.get('file') == 'persone.json'
-    # assert vars.get('weekday') == 'saturday'
-    # assert vars.get('number') == 3
-
+PATH = 'groups/tests/members_example.json'
 
 def test_allmembers_init(capsys):
+    """Unit tests for the __init__ method in AllMembers()"""
     gr.AllMembers()
-    out, err = capsys.readouterr()
+    out, _ = capsys.readouterr()
     assert out == 'Must provide a file path, returning empty list\n'
 
     gr.AllMembers('notfound')
-    out, err = capsys.readouterr()
+    out, _ = capsys.readouterr()
     assert out == 'Could not find the file, returning empty list\n'
 
 
 def test_allmembers_members():
-    path = 'tests/members_example.json'
-    allmembers = gr.AllMembers(path)
+    """Unit tests for the members method in AllMembers()"""
+    allmembers = gr.AllMembers(PATH)
 
     assert len(allmembers.get_members()) == 14
 
@@ -50,8 +44,8 @@ def test_allmembers_members():
 
 
 def test_allmembers_groups():
-    path = 'tests/members_example.json'
-    allmembers = gr.AllMembers(path)
+    """Unit tests for the groups method in AllMembers()"""
+    allmembers = gr.AllMembers(PATH)
 
     random.seed(12345)
 
@@ -66,40 +60,41 @@ def test_allmembers_groups():
 
 
 def test_get_groups_list():
-    path = 'tests/members_example.json'
-    allmembers = gr.AllMembers(path)
+    """Unit tests for the get_groups as list method in AllMembers()"""
+    allmembers = gr.AllMembers(PATH)
 
     random.seed(12345)
 
     assert isinstance(allmembers.get_groups_list(), list)
     assert isinstance(allmembers.get_groups_list(5), list)
-    m = allmembers.get_groups_list(3, 'wednesday')
-    assert isinstance(m, list)
-    assert ['Stephen & Lyda', 'Zena'] in m
-    assert len(m) == 6
-    n = allmembers.get_groups_list(5, 'saturday')
-    assert len(n) == 3
-    assert ['Melissia', 'Stephen & Lyda', 'Jose & Dionne'] in n
+    each = allmembers.get_groups_list(3, 'wednesday')
+    assert isinstance(each, list)
+    assert ['Stephen & Lyda', 'Zena'] in each
+    assert len(each) == 6
+    them = allmembers.get_groups_list(5, 'saturday')
+    assert len(them) == 3
+    assert ['Melissia', 'Stephen & Lyda', 'Jose & Dionne'] in them
 
 
 def test_printable_groups():
-    path = 'tests/members_example.json'
-    allmembers = gr.AllMembers(path)
+    """Unit tests for the printable groups method in AllMembers()"""
+    allmembers = gr.AllMembers(PATH)
 
     random.seed(12345)
 
     assert isinstance(allmembers.printable_groups(), list)
     assert isinstance(allmembers.printable_groups(6), list)
-    m = allmembers.printable_groups(3, 'wednesday')
-    assert isinstance(m, list)
-    assert 'Stephen & Lyda, Zena' in m
-    assert len(m) == 6
-    n = allmembers.printable_groups(5, 'saturday')
-    assert len(n) == 3
-    assert 'Melissia, Stephen & Lyda, Jose & Dionne' in n
+    each = allmembers.printable_groups(3, 'wednesday')
+    assert isinstance(each, list)
+    assert 'Stephen & Lyda, Zena' in each
+    assert len(each) == 6
+    them = allmembers.printable_groups(5, 'saturday')
+    assert len(them) == 3
+    assert 'Melissia, Stephen & Lyda, Jose & Dionne' in them
 
 
 def test_get_next_weekday():
+    """Unit tests for the weekday iterator in WeekdaysFinder()"""
     days = gr.WeekdaysFinder()
 
     assert isinstance(days.get_next_weekday(3), datetime.date)
@@ -123,6 +118,7 @@ def test_get_next_weekday():
 
 
 def test_generator_weekday():
+    """Unit tests for the weekday generator in WeekdaysFinder()"""
     days = gr.WeekdaysFinder()
 
     assert isinstance(days.generator_weekday('saturday'), collections.Generator)
@@ -142,9 +138,9 @@ def test_generator_weekday():
     assert next(dates_gen) == datetime.date(2017, 12, 2)
 
 
-def test_MonthCalendarGroups(capsys):
-    path = 'tests/members_example.json'
-    members = gr.AllMembers(path)
+def test_class_monthcalendargroups(capsys):
+    """Unit tests for the class MonthCalendarGroups()"""
+    members = gr.AllMembers(PATH)
 
     nov = gr.MonthCalendarGroups(members, 'november').full_calendar
     assert isinstance(nov, list)
@@ -155,21 +151,22 @@ def test_MonthCalendarGroups(capsys):
     assert 'Saturday' in out
 
 def test_main(mocker, capsys):
+    """Unit tests for the main() function in the groups module"""
     mocker.patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(
-                output_type='groups',
-                members_json='tests/members_example.json',
-                weekday = 'wednesday',
-                number = 4))
+                 return_value=argparse.Namespace(
+                     output_type='groups',
+                     members_json='groups/tests/members_example.json',
+                     weekday='wednesday',
+                     number=4))
     gr.main()
     out, _ = capsys.readouterr()
     assert 'Stephen & Lyda' in out
 
     mocker.patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(
-                output_type='month',
-                members_json='tests/members_example.json',
-                month = 'november'))
+                 return_value=argparse.Namespace(
+                     output_type='month',
+                     members_json='groups/tests/members_example.json',
+                     month='november'))
     gr.main()
     out, _ = capsys.readouterr()
     assert 'Wednesday' in out
